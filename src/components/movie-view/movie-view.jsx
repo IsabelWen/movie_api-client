8a8d7323a97738bee83565e3208d8acc66f97ad6
Isy <isy1997@freenet.de>
2023-11-20 19:10:34 +0100

    Adjust MovieView component to myMovie API

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,27 +2,27 @@ export const MovieView = ({ movie, onBackClick }) => {
     return (
         <div>
             <div>
-                <img src={movie.image} alt="movie cover"/>
+                <img src={movie.ImagePath} alt="movie cover"/>
             </div>
             <div>
                 <span>Title: </span>
-                <span>{movie.title}</span>
+                <span>{movie.Title}</span>
             </div>
             <div>
                 <span>Description: </span>
-                <span>{movie.description}</span>
+                <span>{movie.Description}</span>
             </div>
             <div>
                 <span>Director: </span>
-                <span>{movie.director}</span>
+                <span>{movie.Director.Name}</span>
             </div>
             <div>
                 <span>Genre: </span>
-                <span>{movie.genre}</span>
+                <span>{movie.Genre.Name}</span>
             </div>
             <div>
                 <span>Year: </span>
-                <span>{movie.year}</span>
+                <span>{movie.Year}</span>
             </div>
             <button onClick={onBackClick}>Back</button>
         </div>
