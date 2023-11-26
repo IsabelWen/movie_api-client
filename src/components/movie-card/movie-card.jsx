e97c1720289b98b0ef6f772f6a3fcc1d00657c92
Isy <isy1997@freenet.de>
2023-11-26 12:09:41 +0100

    Add classes for movie-card.scss

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,11 +1,12 @@
 import PropTypes from "prop-types";
+import "./movie-card.scss";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
-        <Card className="h-100 shadow-lg">
-            <Card.Img variant="top" src={movie.ImagePath} />
+        <Card className="h-100 mt-5 card-shadow">
+            <Card.Img variant="top card-img" src={movie.ImagePath} />
             <Card.Body>
                 <Card.Title>{movie.Title}</Card.Title>
                 <Card.Text>{movie.Director.Name}</Card.Text>
