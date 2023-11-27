27cc5693b1e043c1723a3e8918eea2239545de74
Isy <isy1997@freenet.de>
2023-11-27 16:12:50 +0100

    Replace onMovieClick with React Router link in MovieCard

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,18 +1,20 @@
 import PropTypes from "prop-types";
 import "./movie-card.scss";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export const MovieCard = ({ movie, onMovieClick }) => {
+export const MovieCard = ({ movie }) => {
     return (
         <Card className="h-100 mt-5 card-shadow">
             <Card.Img variant="top card-img" src={movie.ImagePath} />
             <Card.Body>
                 <Card.Title>{movie.Title}</Card.Title>
                 <Card.Text>{movie.Director.Name}</Card.Text>
-                <Button onClick={() => onMovieClick(movie)} variant="link">
-                    Open
-                </Button>
+                <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
+                    <Button variant="link">
+                        Open
+                    </Button>
+                </Link>
             </Card.Body>
         </Card>
     );
@@ -22,6 +24,5 @@ export const MovieCard = ({ movie, onMovieClick }) => {
 MovieCard.propTypes = {
     movie: PropTypes.shape({
         Title: PropTypes.string,        
-    }).isRequired,
-    onMovieClick: PropTypes.func.isRequired
+    }).isRequired
 };
