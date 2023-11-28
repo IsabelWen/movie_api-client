6b67bfb7ec1f222c9f3cef42c0c4c652e60ca042
Isy <isy1997@freenet.de>
2023-11-28 21:06:19 +0100

    Fix bug

--- src/components/profile-view/favorite-movie.jsx
+++ src/components/profile-view/favorite-movie.jsx
@@ -1,21 +1,27 @@
 import { MovieCard } from "../movie-card/movie-card";
 import { Row, Col, Button } from "react-bootstrap";
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-export const FavoriteMovies = ({ favoriteMovieList }) => {
+export const FavoriteMovies = ({ favoriteMovieList, removeFav }) => {
     return (
         <>
             <h2>Favorite Movies</h2>
                 <Row className="justify-content-right justify-content-md-center">
-                    {favoriteMovieList.map((movie) => (
+                    {
+                    favoriteMovieList?.length !== 0 ?
+                    favoriteMovieList?.map((movie) => (
                         <Col sm={6} md={4} lg={3} xl={2} className="mx-2 my-3 col-7 similar-movies-img" key={movie._id}>
                             <MovieCard
                                 movie={movie}
                             />
-                            <Button variant="secondary" onClick={() => removeFav(movies._id)}>Remove from list</Button>
+                            <Button variant="secondary" className="my-4" onClick={() => removeFav(movie._id)}>Remove from list</Button>
                         </Col>
-                    ))}
+                    ))
+                    : <Col>
+                    <p>There are no favorites Movies</p>
+                    </Col>
+                    }
                 </Row>
         </>
-    )
+    );
 }
