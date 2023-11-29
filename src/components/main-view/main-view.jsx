6969b8e2ed93b2eb942024372b6dff7e781519f2
Isy <isy1997@freenet.de>
2023-11-29 16:19:36 +0100

    Remove old comments

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -103,7 +103,7 @@ export const MainView = () => {
                                 {!user ? (
                                     <Navigate to="/login" replace />
                                 ) : movies.length === 0 ? (
-                                    <Col>The list is empty</Col>
+                                    <Col>There is no movie</Col>
                                 ) : (
                                     <Col md={8}>
                                         <MovieView movies={movies} />
@@ -157,86 +157,4 @@ export const MainView = () => {
     );
 };
 
-    /*if (!user) {
-        return (
-            <Row className="justify-content-md-center mt-5">
-                <Col md={5}>
-                    <LoginView 
-                        onLoggedIn={(user, token) => {
-                            setUser(user);
-                            setToken(token);
-                        }}
-                    />
-                    or
-                    <SignupView />
-                </Col>
-            </Row>
-        );
-    }
 
-    // Show Movie Info (MovieView) with similar Movies 
-    if (selectedMovie) {
-        let similarMovies = movies.filter((movie) => 
-        {
-            return movie._id !== selectedMovie._id && movie.Genre.Name === selectedMovie.Genre.Name;
-        });
-        if(similarMovies.length === 0) {
-            return (
-                <Row className="justify-content-md-center">
-                    <Col md={8}>
-                        <MovieView movie={selectedMovie} onBackClick={() => setselectedMovie(null)} /><br />
-                        <h2>Similar Movies</h2>
-                        <p>There are no similar movies.</p>
-                    </Col>
-                </Row>
-            );
-        } else {
-            return (
-                <>
-                <MovieView movie={selectedMovie} onBackClick={() => setselectedMovie(null)} /><br />
-                <h2>Similar Movies</h2>
-                <Row className="justify-content-right justify-content-md-center">
-                    {similarMovies.slice(0,5).map((movie) => (
-                        <Col sm={6} md={4} lg={3} xl={2} className="mx-2 my-3 col-7 similar-movies-img" key={movie._id}>
-                            <MovieCard
-                                movie={movie}
-                                onMovieClick={(newSelectedMovie) => {
-                                    setselectedMovie(newSelectedMovie);
-                                }}
-                            />
-                        </Col>
-                    ))}
-                </Row>
-                </>  
-            );
-        }
-    }
-
-    // Return empty list and Logout Button
-    if (movies.length === 0) {
-        return (
-            <Row className="justify-content-md-center">
-                <Col>
-                    <p>The list is empty!</p>
-                    <Button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</Button>
-                </Col>
-            </Row>
-        );
-    }
-
-    // Return MovieCards and Logout Button
-    return (
-        <Row className="justify-content-center">
-            {movies.map((movie) => (
-                <Col md={6} lg={4} xl={3} className="mb-5 col-8" key={movie._id}>
-                    <MovieCard
-                        movie={movie}
-                        onMovieClick={(newSelectedMovie) => {
-                            setselectedMovie(newSelectedMovie);
-                        }}
-                    />
-                </Col>
-            ))}
-            <Button className="my-5" onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</Button>
-        </Row>
-    );*/
