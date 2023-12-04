18a797d7286fe13aff4456295f6dd5987fb97f26
Isy <isy1997@freenet.de>
2023-12-04 22:12:41 +0100

    Pull

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,11 +2,21 @@ import PropTypes from "prop-types";
 import "./movie-card.scss";
 import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { BookmarkHeart, BookmarkHeartFill } from "react-bootstrap-icons";
 
 export const MovieCard = ({ movie, addFav, removeFav, isFavorite }) => {
     return (
         <Card className="h-100 mt-5 card-shadow">
-            <Card.Img variant="top card-img" src={movie.ImagePath} />
+            <div className="position-relative .d-inline-block">
+                <Card.Img variant="top card-img" src={movie.ImagePath} />
+                <div>
+                    {isFavorite ? (
+                        <BookmarkHeartFill size={40} color="orange" className="fav-button mt-2 me-2 top-0 end-0" onClick={() => removeFav(movie._id)}/>
+                    ) : (
+                        <BookmarkHeart size={40} color="orange" className="fav-button mt-2 me-2 top-0 end-0" onClick={() => addFav(movie._id)}/>
+                    )}
+                </div>
+            </div>
             <Card.Body>
                 <Card.Title>{movie.Title}</Card.Title>
                 <Card.Text>{movie.Director.Name}</Card.Text>
@@ -15,13 +25,6 @@ export const MovieCard = ({ movie, addFav, removeFav, isFavorite }) => {
                         Open
                     </Button>
                 </Link>
-                <div>
-                    {isFavorite ? (
-                        <Button className="my-2 me-2" onClick={() => removeFav(movie._id)}>Remove from Favorite</Button>
-                    ) : (
-                        <Button className="my-2 me-2" onClick={() => addFav(movie._id)}>Add to Favorite</Button>
-                    )}
-                </div>
             </Card.Body>
         </Card>
     );
