1372d5b382f0f60054905dea5e7e9473bf5b1f4b
Isy <isy1997@freenet.de>
2024-01-01 18:24:33 +0100

    Build with parcel

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -27228,7 +27228,7 @@ const MainView = ()=>{
     ]);
     // Add Favorite Movie
     const addFav = (id)=>{
-        fetch(`https://my-movies-api-23e4e5dc7a5e.herokuapp.com/users/${user.Username}/movies/${id}`, {
+        fetch(`https://movieapionrender.onrender.com/users/${user.Username}/movies/${id}`, {
             method: "POST",
             headers: {
                 Authorization: `Bearer ${token}`
@@ -27248,7 +27248,7 @@ const MainView = ()=>{
     };
     // Remove Favorite Movie
     const removeFav = (id)=>{
-        fetch(`https://my-movies-api-23e4e5dc7a5e.herokuapp.com/users/${user.Username}/movies/${id}`, {
+        fetch(`https://movieapionrender.onrender.com/users/${user.Username}/movies/${id}`, {
             method: "DELETE",
             headers: {
                 Authorization: `Bearer ${token}`
@@ -47979,7 +47979,7 @@ const ProfileView = ({ user, movies, setUser, removeFav, addFav })=>{
     };
     // Delete User
     const handleDelete = ()=>{
-        fetch(`https://my-movies-api-23e4e5dc7a5e.herokuapp.com/users/${user.Username}`, {
+        fetch(`https://movieapionrender.onrender.com/users/${user.Username}`, {
             method: "DELETE",
             headers: {
                 Authorization: `Bearer ${token}`
